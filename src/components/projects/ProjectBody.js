791130ad720b1c70148c84008632e90111edd0f2
bhhan <bhhan@bhhanui-MacBookPro.local>
2021-09-13 16:23:03 +0900

    projects skills removed

--- src/components/projects/ProjectBody.js
+++ src/components/projects/ProjectBody.js
@@ -3,30 +3,12 @@ import {Link} from 'react-router-dom';
 import removeDayFromDate from "../../utils/removeDayFromDate";
 
 const ProjectItem = ({mode, project}) => {
-    const skills = project.skills.map(skill => {
-        let skillColor;
-
-        if (skill.use === 'WORK') {
-            skillColor = 'work'
-        } else if (skill.use === 'TOY') {
-            skillColor = 'toy'
-        } else {
-            skillColor = 'study'
-        }
-
-        return (<div className={`project-item__skill ${skillColor}`} key={skill.id}>
-            {skill.name}
-        </div>)
-    });
     return (
         <Link className={mode === true ? "project-item" : "project-item dark"} to={`/projects/${project.id}`}>
             <div className="project-item__image">
                 <img src={project.images[0].path}/>
             </div>
             <h3 className="project-item__title">{project.title}</h3>
-            <ul className="project-item__skills">
-                {skills}
-            </ul>
             <p className="project-item__text">{removeDayFromDate(project.timeRange.startDate)} ~ {removeDayFromDate(project.timeRange.endDate)}</p>
         </Link>
     );
